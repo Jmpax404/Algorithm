b0edba7a9dfaf74041bec0b93741ad131b87c1e2
Jmpax404 <jmpax6@gmail.com>
2023-01-11 18:32:38 +0800

    a

---
+++ C/eg5.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+main(){
+	float f=1.1f;
+	scanf("%g",&f);
+	printf("%.20f\n",f);
+}
+
+//	short x=12;
+//	printf("%hd\n",~x);
+
+
+
+
+
+//	int a,b;
+//	printf("Please input a and b in integer divided by one space:");
+//	while(scanf("%d %d",&a,&b)!=2){
+//		printf("Input error, please try again!\n");
+//		fflush(stdin);
+//		printf("Please input a and b in integer divided by one space:");
+//	}
+//	printf("The max number is %d\n",a>b?a:b);
+
+
+
+
+
+
+
+//	int a=1;
+//	if(0=a){
+//		
+//	}
+
+
+
+
+
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//
+//#define EPS 1e-6
+//
+//int main(){
+//	float f;
+//	scanf("%f",&f);
+//	if (fabs(f)<=EPS ){
+//		printf("is 0\n");
+//	}else{
+//		printf("%.20f\n",f);
+//	}
+//	return 0;
+//}
+
